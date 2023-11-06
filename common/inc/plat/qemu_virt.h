50657b0054219d6991074903345524a78d9da5b8
Henrik Akira Karlsson <hakarlsson@proton.me>
2023-11-06 13:48:17 +0100

    Adds kernel assert statements

--- common/inc/plat/qemu_virt.h
+++ common/inc/plat/qemu_virt.h
@@ -28,7 +28,7 @@
 #define INIT_CAPS                                                     \
 	{                                                             \
 		[0] = cap_mk_pmp(0x20005fff, MEM_RWX),                \
-		[1] = cap_mk_memory(0x80020000, 0x80100000, MEM_RWX), \
+		[1] = cap_mk_memory(0x80020000, 0x88000000, MEM_RWX), \
 		[2] = cap_mk_memory(0x10000000, 0x10001000, MEM_RW),  \
 		[3] = cap_mk_memory(0x200b000, 0x200c000, MEM_R),     \
 		[4] = cap_mk_time(0, 0, S3K_SLOT_CNT),                \
