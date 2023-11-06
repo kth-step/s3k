f94b7a0cbc89cfe921eb469290b78d19e0223353
Henrik Akira Karlsson <hakarlsson@proton.me>
2023-11-06 11:38:25 +0100

    Adds more descriptive QEMU script

--- common/plat/qemu_virt.mk
+++ common/plat/qemu_virt.mk
@@ -1,11 +1,7 @@
 export ARCH=rv64imac_zicsr
 export ABI=lp64
 export CMODEL=medany
-export QEMU_MACHINE=virt
-export QEMU_SMP=1
-export QEMU_OPTIONS=-icount 3
 export COMMON_INC:=${ROOT}/common/inc
 export COMMON_LIB:=${ROOT}/common/build/${PLATFORM}
 export STARTFILES:=${ROOT}/common/build/${PLATFORM}/start
 PLAT_SRCS=src/drivers/uart/ns16550a.c src/drivers/time.c
-$(info Platform qemu_virt uses hart 0 only. If you want multiple cores, use qemu_virt4.)
--- common/plat/qemu_virt4.mk
+++ common/plat/qemu_virt4.mk
@@ -1,8 +1,6 @@
 export ARCH=rv64imac_zicsr
 export ABI=lp64
 export CMODEL=medany
-export QEMU_MACHINE=virt
-export QEMU_SMP=4
 export COMMON_INC:=${ROOT}/common/inc
 export COMMON_LIB:=${ROOT}/common/build/${PLATFORM}
 export STARTFILES:=${ROOT}/common/build/${PLATFORM}/start
