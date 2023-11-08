cd536230071b4a2014c2919da8ccfc679889399b
Henrik Akira Karlsson <hakarlsson@proton.me>
2023-11-08 22:39:06 +0100

    Improves userland libraries

--- common/inc/altc/altio.h
+++ common/inc/altc/altio.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stddef.h>
 
 int alt_getchar(void);
 int alt_putchar(char c);
@@ -6,3 +7,4 @@ int alt_putstr(const char *str);
 int alt_puts(const char *str);
 int alt_gets(char *str);
 int alt_printf(const char *fmt, ...);
+int alt_snprintf(char *restrict s, size_t n, char *restrict fmt, ...);
