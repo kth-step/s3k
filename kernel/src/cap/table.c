37cb74df2f8eeeeebce974b0dc66de9dbc2a405d
Henrik Karlsson <hakarlsson@proton.me>
2025-02-25 08:01:12 +0100

    Fixes kernel - platform configuration.

--- kernel/src/cap/table.c
+++ kernel/src/cap/table.c
@@ -1,20 +1,10 @@
 #include "cap/table.h"
 
+#include "cap/init.h"
 #include "cap/util.h"
 #include "kassert.h"
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
-
-#define INIT_CAPS                                                     \
-	{                                                             \
-		[0] = cap_mk_pmp(0x20005fff, MEM_RWX),                \
-		[1] = cap_mk_memory(0x80020000, 0x88000000, MEM_RWX), \
-		[2] = cap_mk_memory(0x10000000, 0x10001000, MEM_RW),  \
-		[3] = cap_mk_memory(0x200b000, 0x200c000, MEM_R),     \
-		[4] = cap_mk_time(0, 0, NSLOT),                \
-		[8] = cap_mk_monitor(0, NPROC),                \
-		[9] = cap_mk_channel(0, NCHAN),                \
-	}
 
 struct cte {
 	uint32_t prev, next;
