4afe3c5aab529dc19b832443e779059d3350010e
Henrik Karlsson <hakarlsson@proton.me>
2025-02-25 08:13:09 +0100

    Fixes vsnprintf,snprintf,vsprintf,sprintf.

--- libaltc/src/printf.c
+++ libaltc/src/printf.c
@@ -21,7 +21,7 @@ struct buffer_file {
 	size_t size;
 };
 
-int buffer_fputchar(int c, ALTFILE *f)
+static int buffer_fputchar(int c, ALTFILE *f)
 {
 	struct buffer_file *bf = (struct buffer_file*)f;
 	if (bf->i >= bf->size)
@@ -53,6 +53,13 @@ int alt_sprintf(char *s, const char *fmt, ...)
 	return length;
 }
 
+int alt_vsprintf(char *s, const char *fmt, va_list ap)
+{
+	struct buffer_file bf = BUFFER_FILE(s, INT_MAX);
+	alt_vfprintf((ALTFILE*)&bf, fmt, ap);
+	return bf.i;
+}
+
 int alt_snprintf(char *s, size_t n, const char *fmt, ...)
 {
 	va_list ap;
@@ -62,16 +69,11 @@ int alt_snprintf(char *s, size_t n, const char *fmt, ...)
 	return length;
 }
 
-int alt_vsprintf(char *s, const char *fmt, va_list ap)
-{
-	struct buffer_file b = BUFFER_FILE(s, INT_MAX);
-	return alt_vfprintf((ALTFILE*)&b, fmt, ap);
-}
-
 int alt_vsnprintf(char *s, size_t n, const char *fmt, va_list ap)
 {
-	struct buffer_file b = BUFFER_FILE(s, n);
-	return alt_vfprintf((ALTFILE*)&b, fmt, ap);
+	struct buffer_file bf = BUFFER_FILE(s, n);
+	alt_vfprintf((ALTFILE*)&bf, fmt, ap);
+	return bf.i;
 }
 
 int alt_fprintf(ALTFILE *f, const char *fmt, ...)
@@ -85,122 +87,6 @@ int alt_fprintf(ALTFILE *f, const char *fmt, ...)
 
 int alt_vfprintf(ALTFILE *f, const char *fmt, va_list ap)
 {
+	/* TODO */
 	return 0;
 }
-
-/*
-
-#define ALT_PRINTF_BUF_SIZE 128
-
-static char *write_dec(char *restrict dst, const char *end,
-		       unsigned long long val)
-{
-	if (!val && dst != end) {
-		*(dst++) = '0';
-	} else if (dst != end) {
-		int i = 0;
-		char buf[24];
-		while (val) {
-			int tmp = val % 10;
-			buf[i++] = '0' + tmp;
-			val /= 10;
-		}
-		while (i > 0 && dst != end)
-			*(dst++) = buf[--i];
-	}
-	return dst;
-}
-
-static char *write_hex(char *restrict dst, const char *end,
-		       unsigned long long val)
-{
-	if (!val && dst != end) {
-		*(dst++) = '0';
-	} else if (dst != end) {
-		int i = 0;
-		char buf[16];
-		while (val) {
-			int tmp = val & 0xF;
-			buf[i++] = tmp < 10 ? ('0' + tmp) : 'A' + (tmp - 10);
-			val >>= 4;
-		}
-		while (i > 0 && dst != end)
-			*(dst++) = buf[--i];
-	}
-	return dst;
-}
-
-static char *write_str(char *restrict dst, const char *end, char *restrict src)
-{
-	while (dst != end && *src != '\0')
-		*(dst++) = *(src++);
-	return dst;
-}
-
-static char *write_char(char *restrict dst, const char *end, char c)
-{
-	if (dst != end)
-		*(dst++) = c;
-	return dst;
-}
-
-int alt_vsnprintf(char *restrict str, size_t size, const char *restrict fmt,
-		  va_list ap)
-{
-	char *s = str;
-	const char *end = str + size - 1;
-	while (*fmt != '\0' && s != end) {
-		if (*(fmt++) != '%') {
-			s = write_char(s, end, *(fmt - 1));
-			continue;
-		}
-		switch (*(fmt++)) {
-		case '%':
-			s = write_char(s, end, '%');
-			break;
-		case 'c':
-			s = write_char(s, end, va_arg(ap, int));
-			break;
-		case 's':
-			s = write_str(s, end, va_arg(ap, char *));
-	n		break;
-		case 'x':
-			s = write_hex(s, end, va_arg(ap, unsigned int));
-			break;
-		case 'X':
-			s = write_hex(s, end, va_arg(ap, unsigned long));
-			break;
-		case 'd':
-			s = write_dec(s, end, va_arg(ap, unsigned int));
-			break;
-		case 'D':
-			s = write_dec(s, end, va_arg(ap, unsigned long));
-			break;
-		case '\0':
-			break;
-		}
-	}
-	*s = '\0';
-	return s - str;
-}
-
-int alt_snprintf(char *restrict str, size_t size, const char *restrict fmt, ...)
-{
-	va_list ap;
-	va_start(ap, fmt);
-	int len = alt_vsnprintf(str, size, fmt, ap);
-	va_end(ap);
-	return len;
-}
-
-int alt_printf(const char *restrict fmt, ...)
-{
-	char buf[ALT_PRINTF_BUF_SIZE];
-	va_list ap;
-	va_start(ap, fmt);
-	int len = alt_vsnprintf(buf, ALT_PRINTF_BUF_SIZE, fmt, ap);
-	va_end(ap);
-	alt_putstr(buf);
-	return len;
-}
-*/
