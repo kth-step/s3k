d2c359f11c7e681b0db8a05d235f3b7e02bb1416
Henrik Karlsson <hakarlsson@proton.me>
2025-03-03 15:30:23 +0100

    Restructure kernel code.

--- libaltc/include/altc/init.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void alt_init(void);
--- tools.mk
+++
@@ -1,8 +0,0 @@
-RISCV_PREFIX?=riscv64-unknown-elf-
-export CC     :=${RISCV_PREFIX}gcc
-export AR     :=${RISCV_PREFIX}ar
-export LD     :=${RISCV_PREFIX}ld
-export SIZE   :=${RISCV_PREFIX}size
-export OBJDUMP:=${RISCV_PREFIX}objdump
-export OBJCOPY:=${RISCV_PREFIX}objcopy
-export GDB    :=${RISCV_PREFIX}gdb
