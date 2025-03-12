e39b406be42900357f069c13b344f039b3c3bef8
Henrik Karlsson <hakarlsson@proton.me>
2025-03-12 16:35:41 +0100

    Corrects RAM memory capability for qemu.

--- platform/qemu_virt/config.c
+++ platform/qemu_virt/config.c
@@ -10,7 +10,7 @@ cap_t cap_init(int i)
 {
 	cap_t init_caps[] = {
 	    [0] = cap_mk_pmp(0x20005fff, MEM_RWX),
-	    [1] = cap_mk_memory(0x80020000, 0x88000000, MEM_RWX),
+	    [1] = cap_mk_memory(0x80010000, 0x88000000, MEM_RWX),
 	    [2] = cap_mk_memory(0x10000000, 0x10001000, MEM_RW),
 	    [3] = cap_mk_memory(0x200b000, 0x200c000, MEM_R),
 	    [4] = cap_mk_time(0, 0, NSLOT),
