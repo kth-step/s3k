41f0194cd3e9938d8d668ccdf86d29bc22bb0ead
Henrik Karlsson <hakarlsson@proton.me>
2025-03-21 14:09:49 +0100

    Set cheshire to single core.

--- platform/cheshire/config.c
+++ platform/cheshire/config.c
@@ -9,12 +9,9 @@ cap_t cap_init(int i)
 	    [2] = cap_mk_memory(0x03002000, 0x03003000, MEM_RW),
 	    [3] = cap_mk_memory(0x204b000, 0x204c000, MEM_R),
 	    [4] = cap_mk_time(0, 0, NSLOT),
-	    [5] = cap_mk_time(1, 0, NSLOT),
-	    [6] = cap_mk_time(2, 0, NSLOT),
-	    [7] = cap_mk_time(3, 0, NSLOT),
-	    [8] = cap_mk_monitor(0, NPROC),
-	    [9] = cap_mk_channel(0, NCHAN),
-	    [10] = {.raw = 0},
+	    [5] = cap_mk_monitor(0, NPROC),
+	    [6] = cap_mk_channel(0, NCHAN),
+	    [7] = {.raw = 0},
 	};
 
 	return init_caps[i];
